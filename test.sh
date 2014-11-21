7be2aa91eb945b0254ddee3b8e28a7261be3c9d4
= <alex@alexgeek.co.uk>
2014-11-21 23:31:45 +0000

    Aquila script fixes

--- test.sh
+++ test.sh
@@ -4,14 +4,11 @@ INC=${2:-1}
 FINISH=${3:-8}
 N=${4:-4098}
 echo "Testing with threading in range $START-$FINISH in increments of $INC for $N by $N array."
-OUTPUT="time"
-TIMES=(real user sys)
-t=${TIMES[0]}
-O="$OUTPUT.$t.log"
->$O
+OUTPUT="time.log"
+>$OUTPUT
 for i in `seq $START $INC $FINISH`; do
   for r in `seq 1 100`; do
-    (time ./relax -n $N -t $i)  2>&1 > /dev/null | grep $t | awk '{print $2}'|sed "s/^[0-9]\+m//" | xargs printf "$i\t%s\n" >> $O
+    (time ./relax -n $N -t $i)  2>&1 > /dev/null | grep real | awk '{print $2}'|sed "s/^[0-9]\+m//" | xargs printf "$i\t%s\n" >> $OUTPUT
   done
 done
 
@@ -21,5 +18,5 @@ gnuplot <<- EOF
     #set term dumb
     set term png
     set output "${OUTPUT}.png"
-    plot [$START:$FINISH] "${OUTPUT}.${TIMES[0]}.log" title "Speedup (${TIMES[0]})" with lines
+    plot [$START:$FINISH] "${OUTPUT}" title "Speedup (real)" with lines
 EOF
