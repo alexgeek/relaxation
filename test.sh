5cc352c8e4197837e392233b1848d4f44d2d3717
= <alex@alexgeek.co.uk>
2014-11-21 23:38:54 +0000

    Aquila script fixes

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 START=${1:-1}
 INC=${2:-1}
 FINISH=${3:-8}
-N=${4:-4098}
+N=${4:-2050}
 echo "Testing with threading in range $START-$FINISH in increments of $INC for $N by $N array."
 OUTPUT="time.log"
 >$OUTPUT
 for i in `seq $START $INC $FINISH`; do
-  for r in `seq 1 100`; do
+  for r in `seq 1 5`; do
     (time ./relax -n $N -t $i)  2>&1 > /dev/null | grep real | awk '{print $2}'|sed "s/^[0-9]\+m//" | xargs printf "$i\t%s\n" >> $OUTPUT
   done
 done
